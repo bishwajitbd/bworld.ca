d3fa1780c0fee80729c276e6644381d72863c71d
bishwajitbd <bishwajitbd@gmail.com>
2020-03-02 18:49:11 -0500

    React and Express.js app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Page from './components/page/page';
+//import WebFooter from './components/footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+    <Page />
+    </div>
+  );
+}
+
+export default App;
