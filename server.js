0abc27a5631bafec46a7d8de060651c4178a9a80
bishwajitbd <bishwajitbd@gmail.com>
2020-03-02 21:41:05 -0500

    update

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const nodemailer = require('nodemailer');
-
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const cors = require('cors');
+app.use(cors());
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
