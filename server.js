8c61b5398cbb8c5b2f76091e9d135585c0547e79
bishwajitbd <bishwajitbd@gmail.com>
2020-03-02 20:26:25 -0500

    React and Express.js app

--- server.js
+++ server.js
@@ -66,7 +66,15 @@ smtpTransport.sendMail(mailOptions,
 
 })
 
-
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.post('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log('We are live on port 3001');
